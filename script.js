f1981aea67fe9b510609af9bc5017d2b0312ab70
safan-shaz <pvsafan53@gmail.com>
2024-11-06 13:52:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let screen = document.getElementById('screen');
+function btnclick(value){
+    screen.value += value;
+}
+function clearscreen(){
+    screen.value= "";
+
+}
+function findresult(){
+    var result = eval(screen.value)
+    screen.value = result;
+}
